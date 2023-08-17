0c18a88eae368fdcc35b8cefb76af199062091f6
illegal-instruction-co <machinetherapist2@gmail.com>
2023-08-17 16:55:07 +0300

    set hw

--- service.js
+++ service.js
@@ -7,7 +7,7 @@ const whisper = new Whisper('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
 const micInstance = mic({
     rate: '16000',
     channels: '1',
-    device: 'plughw:0',
+    hw: 'CARD=USBZH11SENC',
     debug: true
 })
 
