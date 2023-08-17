ac8760e38b9ae4d5f3cb08777f8aea15a5eacf52
illegal-instruction-co <machinetherapist2@gmail.com>
2023-08-17 16:32:54 +0300

    debug

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 const Whisper = require('whisper-nodejs')
 const mic = require('mic')
 
-console.log("Running service.js...")
+console.log("Running service.js!")
 
 const whisper = new Whisper('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')
 const micInstance = mic({
